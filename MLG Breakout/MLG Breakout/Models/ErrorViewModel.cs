827463684f3043f2edb25b23c2bcaf7d019a51f1
Michael Ng <mwnmwn07@gmail.com>
2024-03-05 13:01:34 -0500

    Converted the project to a webapp

---
+++ MLG Breakout/MLG Breakout/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MLG_Breakout.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
